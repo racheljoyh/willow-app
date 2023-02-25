727793a9a24dd634a1b669a1e2de55585305e488
Rachel <racheljhumes@gmail.com>
2023-02-25 16:35:22 -0700

    migration change

--- db/migrate/20230131165159_create_listings.rb
+++ db/migrate/20230131165159_create_listings.rb
@@ -4,7 +4,6 @@ class CreateListings < ActiveRecord::Migration[7.0]
       t.string :image
       t.string :avaliable?
       t.integer :price
-      t.belongs_to :listing_info, null: false, foreign_key: true
 
       t.timestamps
     end
