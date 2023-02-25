3b0528653cd7488563f4a6d41296108b1210e985
Rachel <racheljhumes@gmail.com>
2023-02-25 16:55:51 -0700

    migration change

--- db/migrate/20230202224254_remove_foreign_key_from_listings.rb
+++
@@ -1,6 +0,0 @@
-class RemoveForeignKeyFromListings < ActiveRecord::Migration[7.0]
-  def change
-    remove_foreign_key :listings, :listing_infos
-    remove_index :listings, column: :listing_info_id, unique: true
-  end
-end
